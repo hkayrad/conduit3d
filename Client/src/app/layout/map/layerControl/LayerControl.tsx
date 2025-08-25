de09416ea3e04da7579f3ddc9c309d95ab772128
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-08-25 19:58:47 +0300

    feat: add layer controls button and card
    Signed-off-by: Hakan Kayra Doğan <hkayrad@gmail.com>

--- Client/src/app/layout/map/layerControl/LayerControl.tsx
+++ Client/src/app/layout/map/layerControl/LayerControl.tsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
 import "./style/layerControl.css"
 import { ChevronRight, Layers2 } from "lucide-react";
+import { selectMapState, type MapState } from "../mapSlice";
+import { useAppSelector } from "../../../../lib/hooks";
+import { useDispatch } from "react-redux";
 
 export default function LayerControl() {
     const [isControlsOpen, setIsControlsOpen] = useState<boolean>(false);
+
+    const dispatch = useDispatch();
+    const mapState = useAppSelector(selectMapState);
+
+    const handleLayerToggle = (layer: keyof MapState["visibility"]) => {
+        const isVisible = mapState.visibility[layer];
+        dispatch({ type: "map/setMapLayerVisibility", payload: { layer, visible: !isVisible } });
+    }
+
     return (
         <>
-            <div className={`layer-control ${isControlsOpen ? "open" : "closed"}`}>
-                <button
-                    id="layer-control-toggle"
-                    onClick={() => setIsControlsOpen(!isControlsOpen)}>
-                    {isControlsOpen ? <ChevronRight /> : <Layers2 />}
-                </button>
-            </div>
+            <button
+                id="layer-control-toggle"
+                className={isControlsOpen ? "open" : "closed"}
+                onClick={() => setIsControlsOpen(!isControlsOpen)}>
+                {isControlsOpen ? <ChevronRight /> : <Layers2 />}
+            </button>
             <div className={`layer-control-content ${isControlsOpen ? "open" : "closed"}`}>
-                asdas
+                {
+                    Object.keys(mapState.visibility).map((layer) => (
+                        <div key={layer} className="layer-control-item">
+                            <button
+                                onClick={() => handleLayerToggle(layer as keyof MapState["visibility"])}
+                            >
+                                {mapState.visibility[layer as keyof MapState["visibility"]] ? "Hide" : "Show"} {layer}
+                            </button>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
