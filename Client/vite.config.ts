e5349277cd5fce0615b81300c9805381aff54669
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-08-25 09:37:56 +0300

    feat: add client template
    Signed-off-by: Hakan Kayra Doğan <hkayrad@gmail.com>

---
+++ Client/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// import fs from 'fs'; // Removed because 'fs' is not available in Vite's frontend context
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: {
+      key: 'localhost-key.pem',
+      cert: 'localhost.pem',
+    },
+    host: 'localhost',
+    port: 3000
+  }
+})
