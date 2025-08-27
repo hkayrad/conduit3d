4dd794f808ad7db4debd3d7098cbf6394fddd5a1
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-08-28 00:04:44 +0300

    fix: fix the position to header

--- Client/src/app/shared/loader/Loader.tsx
+++ Client/src/app/shared/loader/Loader.tsx
@@ -1,34 +1,13 @@
-import { useCallback, useEffect, useRef } from "react";
 import "./style/loader.css";
 import { LoaderIcon } from "lucide-react";
 import { useAppSelector } from "../../../lib/hooks/reduxHooks";
 import { selectIsDataLoading } from "../../layout/map/mapSlice";
 
 export default function Loader() {
-    const loaderRef = useRef<HTMLDivElement>(null!);
-
-    const isDataLoading = useAppSelector(selectIsDataLoading);
-
-    const handleMouseMove = useCallback((e: any) => {
-        const loader = loaderRef.current;
-        if (loader) {
-            const { clientX, clientY } = e;
-            loader.style.left = `${clientX + 10}px`;
-            loader.style.top = `${clientY + 10}px`;
-        }
-    }, []);
-
-    useEffect(() => {
-        window.addEventListener("mousemove", handleMouseMove);
-
-        return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
-        };
-    }, []);
+    const isDataLoading = useAppSelector(selectIsDataLoading);;
 
     return (
         <div
-            ref={loaderRef}
             className={`loader ${isDataLoading ? "visible" : ""}`}
         >
             <LoaderIcon id="loader-icon" />
