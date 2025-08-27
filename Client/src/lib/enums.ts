4b66356efa43bd655c92878718a2734d96e6a11e
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-08-27 11:48:20 +0300

    feat: add enums

---
+++ Client/src/lib/enums.ts
@@ -0,0 +1,24 @@
+enum AgDirekTipi {
+    BETON = "BETON",
+    AGAC = "AGAC",
+    DEMIR = "DEMIR",
+    GALVDEMIR = "GALV.DEMIR"
+}
+
+enum OgMusDirekTipi {
+    BETON = "BETON",
+    DEMIR = "DEMIR",
+    GALVDEMIR = "GALV.DEMIR"
+}
+
+enum AydDirekTipi {
+    GALVPOLIGON = "GALV.POLIGON"
+}
+
+enum HatCinsi {
+    HAVAI = "HAVAİ",
+    BARA = "BARA",
+    YERALTI = "YERALTI"
+}
+
+export { AgDirekTipi, OgMusDirekTipi, AydDirekTipi, HatCinsi }
