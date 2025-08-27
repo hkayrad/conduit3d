7f03e5fe26792b25eba0391b0bea149a4ad30c4c
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-08-27 23:58:04 +0300

    feat: add loader

--- Client/src/app/App.tsx
+++ Client/src/app/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Header from "./shared/header/Header";
+import Loader from "./shared/loader/Loader";
 
 export default function App() {
 
     return <>
         <Header />
+        <Loader />
         <Outlet />
     </>
 }
