a822325087e8e9106ff8d450223e90daee9eff86
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-08-27 18:02:17 +0300

    style: add scrollbar styling

--- Client/src/app/shared/layerControlDropdown/LayerControlDropdown.tsx
+++ Client/src/app/shared/layerControlDropdown/LayerControlDropdown.tsx
@@ -16,19 +16,22 @@ export default function LayerControlDropdown(props: Props) {
 
     return (
         <div className={`layer-control-dropdown ${isOpen ? "open" : "closed"}`} onClick={() => setIsOpen(!isOpen)}>
-            <div className="left">
-                {icon}
-                <span>{name}</span>
+            <div className="controller">
+                <div className="left">
+                    {icon}
+                    <span>{name}</span>
+                </div>
+                <div className="right">
+                    <button onClick={(e) => {
+                        e.stopPropagation();
+                        toggleLayer();
+                    }}>
+                        {isLayerVisible ? <Eye /> : <EyeClosed />}
+                    </button>
+                    <ChevronDown className={isOpen ? "open" : "closed"} />
+                </div>
             </div>
-            <div className="right">
-                <button onClick={(e) => {
-                    e.stopPropagation();
-                    toggleLayer();
-                }}>
-                    {isLayerVisible ? <Eye /> : <EyeClosed />}
-                </button>
-                <ChevronDown className={isOpen ? "open" : "closed"} />
-            </div>
+            <div onClick={(e) => e.stopPropagation()} className={`content ${isOpen ? "open" : "closed"}`}></div>
         </div>
     )
 }
