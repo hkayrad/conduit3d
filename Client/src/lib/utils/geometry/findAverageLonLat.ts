cfa64b60de51a3de8224ae343841dacf0ef9d532
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-09-05 10:50:23 +0300

    feat: add helper function

---
+++ Client/src/lib/utils/geometry/findAverageLonLat.ts
@@ -0,0 +1,21 @@
+export function findAverageLonLat(feature: GeoJSON.Geometry): [number, number] {
+    if (!feature) return [0, 0];
+
+    console.log(feature);
+
+
+    const coords = feature.type === "Point" ? [feature.coordinates] :
+        feature.type === "LineString" ? feature.coordinates :
+            feature.type === "Polygon" ? feature.coordinates[0] :
+                [];
+
+    if (coords.length === 0) return [0, 0];
+
+    const sum = coords.reduce((acc, coord) => {
+        acc[0] += coord[0];
+        acc[1] += coord[1];
+        return acc;
+    }, [0, 0]);
+
+    return [sum[0] / coords.length, sum[1] / coords.length];
+}
