3b54c202b2a8842ff1481f0fff19c2ecfe77e917
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-09-12 09:15:01 +0300

    fix: remove console log

--- Client/src/app/layout/auth/Login.tsx
+++ Client/src/app/layout/auth/Login.tsx
@@ -42,7 +42,6 @@ export default function Login(): React.ReactNode {
 
         try {
             const response = await AuthApi.login(user);
-            console.log(response);
 
             if (!response.isSuccess) {
                 dispatch(clearUser());
