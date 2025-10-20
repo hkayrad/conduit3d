38a9c1991f19c5f3c48aacae331b7f3ebc5df1fc
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-10-20 13:19:50 +0300

    build: add vitest as dependency

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
+/// <reference types="vitest/config" />
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   server: {
