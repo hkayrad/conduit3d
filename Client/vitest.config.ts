7d30fff8440d6ebadb277ae2edc624224a7663c1
Hakan Kayra Doğan <hkayrad@gmail.com>
2025-10-20 16:37:46 +0300

    build: configure sonar and vitest

--- Client/vitest.config.ts
+++ Client/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        environment: 'jsdom', // or 'happy-dom' for a lighter alternative
         coverage: {
             reporter: ['text', 'lcov']
         }
